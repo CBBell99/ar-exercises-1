0ad21d32b5d1e9c9393cc0e447a811ab26dc11c9
Paige <paigekato@gmail.com>
2017-04-11 18:02:51 -0700

    Adding Files

---
+++ exercises/exercise_5.rb
@@ -0,0 +1,19 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+
+puts "Exercise 5"
+puts "----------"
+
+# Your code goes here ...
+
+@sum = Stores.sum(:annual_revenue)
+@aver = Stores.average(:annual_revenue)
+
+@mill = Stores.where("annual_revenue > 1000000").count
+
+puts @sum
+puts @aver
+puts @mill
---
+++ exercises/exercise_7.rb
@@ -0,0 +1,17 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+
+puts "Exercise 7"
+puts "----------"
+
+# Your code goes here ...
+puts "Please give me a store name:"
+@store_name = gets.chomp
+
+Store.create(name: @store_name)
+puts @store_name.errors.full_messages
